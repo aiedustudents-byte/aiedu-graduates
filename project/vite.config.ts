86d62ad3139b558b75e94ff65910f1ff52327db3
aiedustudents-byte <aiedustudents@gmail.com>
2025-10-29 16:48:07 +0530

    Delete project directory

--- project/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3001, // 👈 yahan apna custom port likho (5173 ke bajaye)
-  },
-});
